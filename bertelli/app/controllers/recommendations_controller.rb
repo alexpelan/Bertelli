6022f36a9b4cef3a6c4dc65b4193ab47d9198d15
Alex Pelan <alexpelan@gmail.com>
2013-04-10 23:55:47 -0500

    Display formatted weekend dates, use .clone when adding weekends to array

--- bertelli/app/controllers/recommendations_controller.rb
+++ bertelli/app/controllers/recommendations_controller.rb
@@ -24,7 +24,7 @@ class RecommendationsController < ApplicationController
   		event_date = Date.new
     	event_date = Date.parse(recommendation["event"]["datetime_local"])
   		if is_the_freakin_weekend(event_date)
-  			#use the date string as the key so we can more easily check whether events are grouped in a weekend
+  		  #use the date string as the key so we can more easily check whether events are grouped in a weekend
   			@weekend_events.store(event_date,recommendation)			
   		end
   		
@@ -33,7 +33,6 @@ class RecommendationsController < ApplicationController
    	#here's where the magic happens. Hardcoding minimum number of events to 2 for now
    	@weekends = group_into_weekends(@weekend_events,2)
    	
-   	
   end
   
   def new
@@ -41,7 +40,8 @@ class RecommendationsController < ApplicationController
   
   #not I18N'ed - assumes a Fri/Sat/Sun weekend
   def is_the_freakin_weekend(date)
-  	if date.wday == 0 || date.wday == 5 || date.wday == 6
+  	day_of_week = date.wday
+  	if day_of_week == 0 || day_of_week == 5 || day_of_week == 6
   		return true
   	else
   		return false
@@ -60,7 +60,7 @@ class RecommendationsController < ApplicationController
   		#if this isn't the first event within a weekend, check if this event is within 2 days of the previous one
   		#if it is, it's part of that weekend (and can't be part of the next weekend, because math)
  			if weekend.size > 0
- 				if event_date - previous_date <= 2
+ 				if event_date - previous_date <= 2			  
  					weekend.push(events[event_date])
  					previous_date = event_date
  				else
@@ -71,7 +71,7 @@ class RecommendationsController < ApplicationController
   		#first time through, or if the current date doesn't fit in the weekend
   		if need_to_create_new_weekend == true
   			if weekend.size >= minimum_number_of_events
-  				weekends.push(weekend)
+  				weekends.push(weekend.clone) #clone because otherwise modifying weekend modifies the value in the array as well
   			end
   			weekend.clear
   			weekend.push(events[event_date])
@@ -90,5 +90,7 @@ class RecommendationsController < ApplicationController
   	
   	return weekends
   end 
+  
+ 
   
 end
--- bertelli/app/helpers/recommendations_helper.rb
+++ bertelli/app/helpers/recommendations_helper.rb
@@ -1,2 +1,24 @@
 module RecommendationsHelper
+
+	#turns a day from the weekend into an end user facing date, eg Friday May 6th-Sunday May 8th
+	def calculate_date_string(date)
+		event_date = Date.parse(date)
+		start_date = Date.new
+		end_date = Date.new
+		if event_date.wday == 5 #friday
+			start_date = event_date
+			end_date = event_date + 2
+		elsif event_date.wday == 6 #saturday
+			start_date = event_date - 1
+			end_date = event_date + 1
+		elsif event_date.wday == 0 #sunday
+			state_date = event_date - 2
+			end_date = event_date
+		end
+		date_string = start_date.to_time.strftime("%m/%d/%Y") + " - " + end_date.to_time.strftime("%m/%d/%Y")
+		
+		return date_string
+	end
+
+
 end
